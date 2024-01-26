fb5a56f38eac110e0375b194dd4c56122d14c90a
Antonio nunes <toninunes@hotmail.com>
2024-01-25 21:43:55 -0300

    Testando o Commit

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,6 +16,7 @@ namespace DesafioFundamentos.Models
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
+            // Testando COMMIT antes de implementar
             Console.WriteLine("Digite a placa do veículo para estacionar:");
         }
 
